f5f16ad7a4f879e182dda82f5890d3218326c2ea
marinas <marinaserranomontes@gmail.com>
2017-03-15 15:49:27 +0100

    Fixing undefined stream type case

--- src/logging.js
+++ src/logging.js
@@ -46,7 +46,7 @@ const updateLogAnalytics = (sessionId, connectionId, apiKey) => {
 
 const initLogAnalytics = (source, sessionId, connectionId, apikey) => {
   const otkanalyticsData = {
-    clientVersion: 'js-vsol-1.0.0',
+    clientVersion: 'js-vsol-1.0.11',
     source,
     componentId: 'acceleratorCore',
     name: 'coreAccelerator',
